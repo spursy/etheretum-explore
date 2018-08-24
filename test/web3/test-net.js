e63471d0a93cdee2069f580bcb8d721c921192da
yangyang <yang.yang@gloritysolutions.com>
2018-08-24 19:30:59 +0800

    add file

--- test/web3/test-net.js
+++ test/web3/test-net.js
@@ -1,11 +1,25 @@
 const config = require('../../common/config');
-const web3 = require('web3');
+const Web3 = require('web3');
+let web3;
+
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+} else {
+    let rpc_url = config.rpc_url;
+    web3 = new Web3(new Web3.providers.HttpProvider(rpc_url));
+}
 
 let Start = async function() {
     try {
-        let rpc_url = config.rpc_url;
-
-
+        web3.eth.getBlockNumber((err, ret) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log(JSON.stringify(ret));
+                
+            }
+        })
+        
     } catch (error) {
         console.log(error.stack);
     }

98d4b68592773e2460de3d5e7cdcf325c514b424
yangyang <yang.yang@gloritysolutions.com>
2018-08-24 18:03:44 +0800

    add project

---
+++ common/config.js
@@ -0,0 +1,5 @@
+let config = {
+    rpc_url: "http://10.35.11.56:8540"
+};
+
+module.exports = config;
---
+++ test/web3/test-net.js
@@ -0,0 +1,14 @@
+const config = require('../../common/config');
+const web3 = require('web3');
+
+let Start = async function() {
+    try {
+        let rpc_url = config.rpc_url;
+
+
+    } catch (error) {
+        console.log(error.stack);
+    }
+}
+
+Start();
